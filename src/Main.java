a84bf8256918956ddada68511fbf06b680c82069
gyampson <sgyampoh005@st.ug.edu.gh>
2025-07-09 12:38:50 -0700

    project setup

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Atinka Meds Inventory System!");
+        // Initialize menu and start application here
+    }
+}
